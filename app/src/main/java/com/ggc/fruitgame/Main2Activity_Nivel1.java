2e6df7a46a879f1d74e51a04dc6677c2622fb10b
NoeGarcia2022 <noegarcia4789@gmail.com>
2022-11-03 14:19:51 -0600

    Agregando Splash Screen y corrigiendo errores

--- app/src/main/java/com/ggc/fruitgame/Main2Activity_Nivel1.java
+++ app/src/main/java/com/ggc/fruitgame/Main2Activity_Nivel1.java
@@ -14,7 +14,6 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.nagv.fruitgame.R;
 
 public class Main2Activity_Nivel1 extends AppCompatActivity {
 
